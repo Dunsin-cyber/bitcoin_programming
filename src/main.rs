041a9065b0b999ac43d22f2c7cf09773880e562a
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-09-01 01:22:56 +0100

    writtent the new method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    let abib = FiniteElement::new(40, 20);
+
+}
+
+struct FiniteElement {
+    num:u32,
+    prime: u32
+}
+
+impl FiniteElement {
+    fn new(num:u32, prime:u32) -> Self {
+            if num >= prime  {
+                panic!("Num {} not in field range 0 to {}", num, prime);
+            }
+        Self {num, prime}
+    }
 }
