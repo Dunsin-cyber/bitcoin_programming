d576f95de0b5382c51f0968892a5f5f0f308ead3
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-09-01 20:33:29 +0100

    written all the finite elemnt object

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-fn main() {
-    println!("Hello, world!");
-    let abib = FiniteElement::new(3, 20);
 
-    println!("Repr func called at {}", abib.repr());
-
-}
-
+#[derive(Clone)]
 struct FiniteElement {
     num:u32,
     prime: u32
@@ -22,4 +16,21 @@ impl FiniteElement {
     fn repr(&self) -> String {
      return  format!( "FieldElement_{}({})", self.prime, self.num);
     }
+
+    fn eq (&self, other: Option<FiniteElement>) -> bool {
+  if let Some(element) = other {
+    self.num == element.num && self.prime == element.prime
+  } else {
+    false
+}
+}
+}
+fn main() {
+    println!("Hello, world!");
+    let element_1 = FiniteElement::new(3, 20);
+    let element_2 = FiniteElement::new(4, 20);
+
+    println!("Repr func called at {}", element_1.repr());
+    println!("Repr func called at {}", element_1.eq(Option::Some(element_2)));
+
 }
