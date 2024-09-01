d2882a33e77cfea2b8aa92341b4889bbdf28c6e2
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-09-01 20:46:44 +0100

    Example 1 done

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,31 @@ impl FiniteElement {
      return  format!( "FieldElement_{}({})", self.prime, self.num);
     }
 
-    fn eq (&self, other: Option<FiniteElement>) -> bool {
+    fn __eq__ (&self, other: Option<FiniteElement>) -> bool {
   if let Some(element) = other {
     self.num == element.num && self.prime == element.prime
   } else {
     false
 }
 }
+//Example 1
+ fn __ne__(&self, other:Option<FiniteElement>) -> bool {
+    if let Some(element) = other {
+        self.num != element.num || self.prime != element.prime
+    } else {
+        false
+    }
+ }
 }
+
 fn main() {
     println!("Hello, world!");
-    let element_1 = FiniteElement::new(3, 20);
-    let element_2 = FiniteElement::new(4, 20);
+    let element_1 = FiniteElement::new(7, 13);
+    let element_2 = FiniteElement::new(6, 13);
 
-    println!("Repr func called at {}", element_1.repr());
-    println!("Repr func called at {}", element_1.eq(Option::Some(element_2)));
+    println!("Repr func called  for element_1 at {}", element_1.repr());
+    println!("Repr func called  for element_2 at {}", element_2.repr());
+    println!("__Eq__ func called {}", element_1.__eq__(Option::Some(element_2.clone())));
+    println!("__Ne__ func called {}", element_1.__ne__(Option::Some(element_2)));
 
 }
