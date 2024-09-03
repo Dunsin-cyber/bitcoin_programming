e0f87b60f75f58bab4e59f9c5a3573e69d3d64a8
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-09-03 23:37:32 +0100

    completed example 2

--- src/chapter_2/exercise_1.rs
+++ src/chapter_2/exercise_1.rs
@@ -7,9 +7,9 @@ use crate::chapter_2::point::Point;
 
 pub fn exercise_1 () {
     let p = Point::new(2,4,5, 7);
-    let q = Point::new(2,4,5, 7);
-    let r= Point::new(2,4,5, 7);
-    let s = Point::new(2,4,5, 7);
+    let q = Point::new(-1,-1,5, 7);
+    let r= Point::new(18,77,5, 7);
+    let s = Point::new(5,7,5, 7);
 
 
     println!("{:?}", p);
